f917ea94e92a24b8442af5a3c6ecfd83a2bcf498
steven harrison <steven@sharriso.adsl24.co.uk>
2011-05-29 21:40:40 +0100

    Added ability for messages to contain colour text. Removed the players IP address being broadcast to everyone. Removed the creepy noise broadcast.

--- load.cpp
+++ load.cpp
@@ -61,8 +61,25 @@ void LoadMessages ()
     fMessages >> sMessageCode >> ws;
     getline (fMessages, sMessageText);
     if (!(sMessageCode.empty ()))
-      messagemap [tolower (sMessageCode)] = 
-            FindAndReplace (sMessageText, "%r", "\n");
+    {
+      sMessageText = FindAndReplace (sMessageText, "%r", "\r\n");
+      sMessageText = FindAndReplace (sMessageText, "[/color]", "\e[0m");
+      sMessageText = FindAndReplace (sMessageText, "[red]", "\e[0;31m");
+      sMessageText = FindAndReplace (sMessageText, "[green]", "\e[0;32m");
+      sMessageText = FindAndReplace (sMessageText, "[yellow]",  "\e[0;33m");
+      sMessageText = FindAndReplace (sMessageText, "[blue]", "\e[0;34m");
+      sMessageText = FindAndReplace (sMessageText, "[magenta]", "\e[0;35m");
+      sMessageText = FindAndReplace (sMessageText, "[cyan]", "\e[0;36m");
+      sMessageText = FindAndReplace (sMessageText, "[white]", "\e[0;37m");
+      sMessageText = FindAndReplace (sMessageText, "[grey]", "\e[1;30m");
+      sMessageText = FindAndReplace (sMessageText, "[RED]", "\e[1;31m");
+      sMessageText = FindAndReplace (sMessageText, "[GREEN]", "\e[1;32m");
+      sMessageText = FindAndReplace (sMessageText, "[YELLOW]", "\e[1;33m");
+      sMessageText = FindAndReplace (sMessageText, "[BLUE]", "\e[1;34m");
+      sMessageText = FindAndReplace (sMessageText, "[MAGENTA]", "\e[1;35m");
+      sMessageText = FindAndReplace (sMessageText, "[CYAN]", "\e[1;36m");
+      sMessageText = FindAndReplace (sMessageText, "[WHITE]", "\e[1;37m");
+      messagemap [tolower (sMessageCode)] = sMessageText;    }
     } // end of read loop
   
 } // end of LoadMessages
