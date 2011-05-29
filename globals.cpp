e49a1fd3732333cfe5f7ec54ac67a20b0ed30801
Octav Sandulescu <narc@hermes.internal.narc.ro>
2011-05-30 00:02:03 +0300

    Fixed code style (hopefully globally, hopefully not too badly)

--- globals.cpp
+++ globals.cpp
@@ -2,32 +2,32 @@
 
  tinymudserver - an example MUD server
 
- Author:  Nick Gammon 
-          http://www.gammon.com.au/ 
+ Author:	Nick Gammon
+					http://www.gammon.com.au/
 
 (C) Copyright Nick Gammon 2004. Permission to copy, use, modify, sell and
 distribute this software is granted provided this copyright notice appears
 in all copies. This software is provided "as is" without express or implied
 warranty, and with no claim as to its suitability for any purpose.
- 
+
 */
 
 // standard library includes ...
 
-using namespace std; 
+using namespace std;
 
 #include "globals.h"
 
 // global variables
-bool   bStopNow = false;      // when set, the MUD shuts down
-time_t tLastMessage = 0;      // time we last sent a periodic message 
-int    iControl = NO_SOCKET;  // socket for accepting new connections 
+bool	 bStopNow = false;			// when set, the MUD shuts down
+time_t tLastMessage = 0;			// time we last sent a periodic message
+int		iControl = NO_SOCKET;	// socket for accepting new connections
 
 // list of all connected players
-tPlayerList playerlist;   
+tPlayerList playerlist;
 // map of all rooms
 tRoomMap roommap;
-// map of known commands (eg. look, quit, north etc.)
+// map of known commands(eg. look, quit, north etc.)
 map<string, tHandler> commandmap;
 // map of things to do for various connection states
 map<tConnectionStates, tHandler> statemap;
--- tinymudserver.cpp
+++ tinymudserver.cpp
@@ -5,7 +5,7 @@
  Author:  Nick Gammon
           http://www.gammon.com.au/
 
- Date:    22nd July 2004
+ Date:      22nd July 2004
 
 (C) Copyright Nick Gammon 2004. Permission to copy, use, modify, sell and
 distribute this software is granted provided this copyright notice appears
@@ -23,45 +23,44 @@ using namespace std;
 #include "constants.h"
 #include "globals.h"
 
-void LoadThings ();
-int InitComms ();
-void MainLoop ();
-void CloseComms ();
+void LoadThings();
+int InitComms();
+void MainLoop();
+void CloseComms();
 
 
 // called approximately every 0.5 seconds - handle things like fights here
-void PeriodicUpdates ()
-  {
-    //      The example below just sends a message every MESSAGE_INTERVAL seconds.
-    // send new command if it is time
-  //if (time (NULL) > (tLastMessage + MESSAGE_INTERVAL))
-  //  {
-  //  SendToAll ("You hear creepy noises ...\n");
-  //  tLastMessage = time (NULL);
-  //  }
-
-  } // end of PeriodicUpdates
-
-
-// main program
-int main ()
+void PeriodicUpdates()
 {
-  cout << "Tiny MUD server version " << VERSION << endl;
+	// The example below just sends a message every MESSAGE_INTERVAL seconds.
 
-  LoadThings ();    // load stuff
+	//if(time(NULL) >(tLastMessage + MESSAGE_INTERVAL))
+	//{
+	//	SendToAll("You hear creepy noises ...\n");
+	//	tLastMessage = time(NULL);
+	//}
 
-  if (InitComms ()) // listen for new connections
-    return 1;
+}
 
-  cout << "Accepting connections from port " <<  PORT << endl;
 
-  MainLoop ();    // handle player input/output
 
-  // game over - tell them all
-  SendToAll ("\n\n** Game shut down. **\n\n");
+int main()
+{
+	cout << "Tiny MUD server version " << VERSION << endl;
 
-  CloseComms ();  // stop listening
+	LoadThings();
 
-  cout << "Game shut down." << endl;
-  return 0;
-}   // end of main
+	if(InitComms())
+		return 1;
+
+	cout << "Accepting connections from port " <<	PORT << endl;
+
+	MainLoop();
+
+	SendToAll("\n\n** Game shut down. **\n\n");
+
+	CloseComms();
+
+	cout << "Game shut down." << endl;
+	return 0;
+}
