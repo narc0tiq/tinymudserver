d463a5b4bb1beda795baf293c837406053a2a289
Octav Sandulescu <github@narc.ro>
2011-06-08 23:05:57 +0300

    Execute() now checks permissions before doing the correct thing, and there's no more code duplication in the tCommand ctors

--- include/command.h
+++ include/command.h
@@ -2,6 +2,7 @@
 #define TINYMUDSERVER_COMMAND_H
 
 #include <map>
+#include <stdexcept>
 
 #include "player.h"
 
@@ -9,22 +10,36 @@ typedef bool(*tPermHandler)(tPlayer * p);
 
 class tCommand
 {
+	private:
+		void GetHelp(string cmdName);
+
 	public:
 		string shorthelp;
 		string longhelp;
 
 		const tPermHandler CanExecute;
-		const tHandler Execute;
+		const tHandler Action;
 
 		tCommand(string sh, string lh, tPermHandler ph, tHandler eh):
-			shorthelp(sh), longhelp(lh), CanExecute(ph), Execute(eh) {}
+			shorthelp(sh), longhelp(lh), CanExecute(ph), Action(eh) {}
 		tCommand(string cmdName, tPermHandler ph, tHandler eh);
 		tCommand(string cmdName, tHandler eh);
+
+		void Execute(tPlayer* p, istream& sArgs)
+		{
+			if(!this->CanExecute(p))
+				throw runtime_error("You cannot do that.");
+
+			this->Action(p, sArgs);
+		}
 };
 
 typedef std::map <string, tCommand*> tCommandMap;
 typedef tCommandMap::const_iterator tCommandMapIterator;
 
 bool AnyCanExecute(tPlayer* p);
+bool TpCanExecute(tPlayer* p);
+bool AdminCanExecute(tPlayer* p);
+bool GaglessCanExecute(tPlayer* p);
 
 #endif
--- source/command.cpp
+++ source/command.cpp
@@ -10,26 +10,42 @@ bool AnyCanExecute(tPlayer* p)
 	return true;
 }
 
-tCommand::tCommand(string cmdName, tPermHandler ph, tHandler eh): CanExecute(ph), Execute(eh)
+bool TpCanExecute(tPlayer* p)
 {
-	map<std::string, string, ciLess>::const_iterator iter;
+	if(!p->HaveFlag("can_tp") and !p->HaveFlag("admin"))
+		return false;
 
-	iter = messagemap.find(cmdName + "-shorthelp");
-
-	if(iter == messagemap.end())
-		this->shorthelp = "No clue what this does.";
-	else
-		this->shorthelp = iter->second;
-
-	iter = messagemap.find(cmdName + "-longhelp");
-
-	if(iter == messagemap.end())
-		this->longhelp = "No idea, sorry.\n";
-	else
-		this->longhelp = iter->second;
+	return true;
 }
 
-tCommand::tCommand(string cmdName, tHandler eh): CanExecute(AnyCanExecute), Execute(eh)
+bool AdminCanExecute(tPlayer* p)
+{
+	if(!p->HaveFlag("admin"))
+		return false;
+
+	return true;
+}
+
+bool GaglessCanExecute(tPlayer* p)
+{
+	if(p->HaveFlag("gagged"))
+		return false;
+
+	return true;
+}
+
+
+tCommand::tCommand(string cmdName, tPermHandler ph, tHandler eh): CanExecute(ph), Action(eh)
+{
+	this->GetHelp(cmdName);
+}
+
+tCommand::tCommand(string cmdName, tHandler eh): CanExecute(AnyCanExecute), Action(eh)
+{
+	this->GetHelp(cmdName);
+}
+
+void tCommand::GetHelp(string cmdName)
 {
 	map<std::string, string, ciLess>::const_iterator iter;
 
