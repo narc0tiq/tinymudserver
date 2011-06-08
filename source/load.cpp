7fccd4a7635e232a8fdbd50c8426656e25fd7ea4
Octav Sandulescu <github@narc.ro>
2011-06-08 21:33:36 +0300

    Removed extraneous LoadRooms and LoadXMLRooms

--- source/load.cpp
+++ source/load.cpp
@@ -80,78 +80,6 @@ void LoadMessages()
 	}
 } // end of LoadMessages
 
-// load rooms and exits
-void LoadRooms()
-{
-	// load rooms file
-	ifstream fRooms(ROOMS_FILE, ios::in);
-	if(!fRooms)
-	{
-		cerr << "Could not open rooms file: " << ROOMS_FILE << endl;
-		return;
-	}
-
-	while(!(fRooms.eof()))
-	{
-		int vnum;
-		fRooms >> vnum;
-		fRooms.ignore(numeric_limits<int>::max(), '\n'); // skip rest of this line
-		string description;
-		getline(fRooms, description);
-
-		// give up if no vnum or description
-		if(vnum == 0 || description.empty())
-			break;
-
-		// get exits
-		string sLine;
-		getline(fRooms, sLine);
-
-		// don't have duplicate rooms
-		if(roommap [vnum] != 0)
-		{
-			cerr << "Room " << vnum << " appears more than once in room file" << endl;
-			continue;
-		}
-
-		tRoom * room = new tRoom(FindAndReplace(description, "%r", "\n") + "\n");
-		roommap [vnum] = room;
-
-		// read exits from line(format is: <dir> <vnum> ...	eg. n 1234 s 5678)
-		istringstream is(sLine);
-		while(is.good())
-		{
-			string dir;
-			int dir_vnum;
-
-			is >> dir;	// direction, eg. n
-			is >> dir_vnum >> ws; // vnum, eg. 1234
-
-			if(is.fail())
-			{
-				cerr << "Bad vnum for exit " << dir << " for room " << vnum << endl;
-				continue;
-			}
-
-			// direction must be valid(eg. n, s, e, w) or it won't be recognised
-			set<string>::const_iterator direction_iter = directionset.find(dir);
-			if(direction_iter == directionset.end())
-			{
-				cerr << "Direction " << dir << " for room " << vnum
-						 << " not in list of directions in control file" << endl;
-				continue;
-			}
-
-			// stop if nonsense
-			if(dir.empty() || dir_vnum == 0)
-				break;
-
-			room->exits [dir] = dir_vnum;	 // add exit
-
-		} // end of getting each direction
-	} // end of read loop
-} // end of LoadRooms
-
 void LoadScreens()
 {
 	string sLine;
@@ -170,11 +98,6 @@ void LoadScreens()
 	}
 }
 
-void LoadXMLRooms()
-{
-	tRoom* core = LoadRoom( INITIAL_ROOM );
-}
-
 // build up our commands map and connection states
 void LoadThings()
 {
@@ -185,8 +108,6 @@ void LoadThings()
 	// load files
 	LoadControlFile();
 	LoadMessages();
-//	LoadRooms();
-//	LoadXMLRooms();
 	LoadScreens();
 
 } // end of LoadThings
